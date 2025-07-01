1f526c71c8d476c2e1957fed7f1427117c546df9
arjsabbir88 <arjsabbir88@gmail.com>
2025-07-02 05:09:09 +0600

    resolved myBooking section plm

--- src/pages/MyBooking/MyBooking.jsx
+++ src/pages/MyBooking/MyBooking.jsx
@@ -64,73 +64,75 @@ const MyBooking = () => {
 
   return (
     <>
-      {myBookings.length === 0 ? (
-        <Fade delay={150} duration={1200} triggerOnce cascade>
-          <div className="text-center font-semibold my-10 min-h-screen">
-            <h1 className="text-3xl text-[#00224D] font-bold">
-              Welcome to TourTide
-            </h1>
-            <p className="text-xs">
-              Your journey begins here – unforgettable adventures, handpicked
-              for every traveler.
-            </p>
-            <div className="my-30 text-red-500">
-              <h3>You have no tour bookings yet!</h3>
-              <Link
-                to="/all-packages"
-                className="btn btn-soft bg-[#FF204E] hover:bg-[#00224D] hover:text-white my-5"
-              >
-                Go For Booking
-              </Link>
-            </div>
-          </div>
-        </Fade>
-      ) : (
-        <>
-          <div className="text-center my-6 min-h-screen">
-            <Fade direction="right" delay={100} duration={1000}>
+      <div className="min-h-screen">
+        {myBookings.length === 0 ? (
+          <Fade delay={150} duration={1200} triggerOnce cascade>
+            <div className="text-center font-semibold my-10 ">
               <h1 className="text-3xl text-[#00224D] font-bold">
                 Welcome to TourTide
               </h1>
-            </Fade>
-            <p className="text-xs">
-              Your journey begins here – unforgettable adventures, handpicked
-              for every traveler.
-            </p>
-          </div>
-          <Fade delay={200} duration={2000} triggerOnce cascade>
-            <div className="overflow-x-auto my-10">
-              <table className="table">
-                {/* head */}
-                {/* <Fade delay={120} duration={1400} triggerOnce cascade> */}
-                <thead>
-                  <tr>
-                    <th>Guide Name</th>
-                    <th>Tour Name</th>
-                    <th>Departure location</th>
-                    <th>Destination</th>
-                    <th>Departure Date</th>
-                    <th>Special Note</th>
-                    <th>Status</th>
-                  </tr>
-                </thead>
-                {/* </Fade> */}
-                {/* <Fade direction="left" delay={200} duration={2000} triggerOnce cascade> */}
-                <tbody>
-                  {myBookings.map((bookings) => (
-                    <BookingRows
-                      key={bookings._id}
-                      bookings={bookings}
-                      onStatusUpdate={handleStatusUpdate}
-                    ></BookingRows>
-                  ))}
-                </tbody>
-                {/* </Fade> */}
-              </table>
+              <p className="text-xs">
+                Your journey begins here – unforgettable adventures, handpicked
+                for every traveler.
+              </p>
+              <div className="my-30 text-red-500">
+                <h3>You have no tour bookings yet!</h3>
+                <Link
+                  to="/all-packages"
+                  className="btn btn-soft bg-[#FF204E] hover:bg-[#00224D] hover:text-white my-5"
+                >
+                  Go For Booking
+                </Link>
+              </div>
             </div>
           </Fade>
-        </>
-      )}
+        ) : (
+          <>
+            <div className="text-center my-6">
+              <Fade direction="right" delay={100} duration={1000}>
+                <h1 className="text-3xl text-[#00224D] font-bold">
+                  Welcome to TourTide
+                </h1>
+              </Fade>
+              <p className="text-xs">
+                Your journey begins here – unforgettable adventures, handpicked
+                for every traveler.
+              </p>
+            </div>
+            <Fade delay={200} duration={2000} triggerOnce cascade>
+              <div className="overflow-x-auto my-10">
+                <table className="table">
+                  {/* head */}
+                  {/* <Fade delay={120} duration={1400} triggerOnce cascade> */}
+                  <thead>
+                    <tr>
+                      <th>Guide Name</th>
+                      <th>Tour Name</th>
+                      <th>Departure location</th>
+                      <th>Destination</th>
+                      <th>Departure Date</th>
+                      <th>Special Note</th>
+                      <th>Status</th>
+                    </tr>
+                  </thead>
+                  {/* </Fade> */}
+                  {/* <Fade direction="left" delay={200} duration={2000} triggerOnce cascade> */}
+                  <tbody>
+                    {myBookings.map((bookings) => (
+                      <BookingRows
+                        key={bookings._id}
+                        bookings={bookings}
+                        onStatusUpdate={handleStatusUpdate}
+                      ></BookingRows>
+                    ))}
+                  </tbody>
+                  {/* </Fade> */}
+                </table>
+              </div>
+            </Fade>
+          </>
+        )}
+      </div>
     </>
   );
 };
